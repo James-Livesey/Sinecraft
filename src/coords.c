efea3197ee5ae0504f7fc45878cb984496cbae4c
James-Livesey <jamesliveg@gmail.com>
2021-11-17 14:10:02 +0000

    Finish vectors/coordinates infrastructure

---
+++ src/coords.c
@@ -0,0 +1,41 @@
+#include <math.h>
+
+#include "coords.h"
+
+#define SQUARE(x) x * x
+
+CartesianVector coords_defaultCartesian() {
+    return CartesianVector {.x = 0, .y = 0, .z = 0};
+}
+
+CartesianVector coords_addCartesian(CartesianVector a, CartesianVector b) {
+    return CartesianVector {.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z};
+}
+
+CartesianVector coords_fromPolar(PolarVector polar) {
+    return CartesianVector {
+        .x = polar.r * sin(polar.incl) * cos(polar.ariz),
+        .y = polar.r * cos(polar.incl),
+        .z = polar.r * sin(polar.incl) * sin(polar.ariz)
+    };
+}
+
+double coords_getMagnitude(CartesianVector vector) {
+    return sqrt(SQUARE(vector.x) + SQUARE(vector.y) + SQUARE(vector.z));
+}
+
+PolarVector coords_defaultPolar() {
+    return PolarVector {.r = 0, .incl = M_PI, .ariz = 0};
+}
+
+PolarVector coords_addPolar(PolarVector a, PolarVector b) {
+    return PolarVector {.r = a.r + b.r, .incl = a.incl + b.incl, .ariz = a.ariz + b.ariz};
+}
+
+PolarVector coords_fromCartesian(CartesianVector cartesian) {
+    return PolarVector {
+        .r = coords_getMagnitude(cartesian),
+        .incl = atan(sqrt(SQUARE(cartesian.x) + SQUARE(cartesian.z)) / cartesian.y),
+        .ariz = cartesian.x == 0 ? M_PI / 2 : atan(cartesian.z / cartesian.x) + (cartesian.x < 0 ? M_PI : 0)
+    };
+}
--- src/coords.h
+++ src/coords.h
@@ -2,15 +2,24 @@
 #define COORDS_H_
 
 typedef struct {
-    float x;
-    float y;
-    float z;
+    double x;
+    double y;
+    double z;
 } CartesianVector;
 
 typedef struct {
-    float r;
-    float incl;
-    float ariz;
-};
+    double r;
+    double incl;
+    double ariz;
+} PolarVector;
+
+CartesianVector coords_defaultCartesian();
+CartesianVector coords_addCartesian(CartesianVector a, CartesianVector b);
+CartesianVector coords_fromPolar(PolarVector polar);
+double coords_getMagnitude(CartesianVector vector);
+
+PolarVector coords_defaultPolar();
+PolarVector coords_addPolar(PolarVector a, PolarVector b);
+PolarVector coords_fromCartesian(CartesianVector cartesian);
 
 #endif
