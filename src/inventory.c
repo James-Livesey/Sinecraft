575b65b7cff4eababbe0e6803e7b9468cd910d98
James-Livesey <jamesliveg@gmail.com>
2021-12-01 18:34:04 +0000

    Add inventory slot movement

--- src/inventory.c
+++ src/inventory.c
@@ -8,6 +8,8 @@
 #include "physics.h"
 
 extern bopti_image_t img_slot;
+extern bopti_image_t img_slotSelected;
+extern bopti_image_t img_slotSource;
 
 Inventory inventory_default() {
     Inventory inventory = {
@@ -34,107 +36,135 @@ void inventory_renderItem(int x, int y, unsigned int type) {
     }
 }
 
-void inventory_renderRow(Inventory inventory, unsigned int offset, int y, int selected) {
-    unsigned int leftmostX = (128 - (SLOTS_IN_ROW * (img_slot.width + 1))) / 2;
+void inventory_renderRow(Inventory inventory, unsigned int offset, int y, int selected, int source) {
+    unsigned int leftmostX = (128 - (SLOTS_IN_ROW * (SLOT_WIDTH + 1))) / 2;
 
     for (unsigned int i = 0; i < SLOTS_IN_ROW; i++) {
-        unsigned int x = leftmostX + ((img_slot.width + 1) * i);
+        int offsetI = (int)(i + offset);
+        unsigned int x = leftmostX + ((SLOT_WIDTH + 1) * i);
+        bool isSelected = offsetI == selected;
+        bool isSource = offsetI == source;
 
-        dimage(x, y, &img_slot);
+        dimage(x - 1, y - 1, isSelected ? &img_slotSelected : (isSource ? &img_slotSource : &img_slot));
 
         if (inventory.slots[i + offset].count > 0 && inventory.slots[i + offset].type != BLOCK_TYPE_AIR) {
             inventory_renderItem(x + 2, y + 2, inventory.slots[i + offset].type);
-        }
-
-        if ((int)(i + offset) == selected) {
-            drect_border(x - 1, y - 1, x + img_slot.width, y + img_slot.height, C_NONE, 2, C_BLACK);
         }
     }
 }
 
 void inventory_renderHotbar(Inventory inventory) {
-    inventory_renderRow(inventory, 0, 63 - img_slot.height, inventory.selectedHotbarSlot);
+    inventory_renderRow(inventory, 0, 63 - SLOT_HEIGHT, inventory.selectedHotbarSlot, -1);
 }
 
-void inventory_renderSurvival(Inventory inventory, int selected) {
+void inventory_renderSurvival(Inventory inventory, int selected, int source) {
     drect(22, 0, 104, 63, C_WHITE);
     dvline(22, C_BLACK);
     dvline(104, C_BLACK);
     dtext(25, 1, C_BLACK, "Inventory");
 
-    const int ROW_OFFSET_HEIGHT = img_slot.height + 1;
+    const int ROW_OFFSET_HEIGHT = SLOT_HEIGHT + 1;
     const int ROW_Y_VALUES[] = {
-        63 - img_slot.height,
+        63 - SLOT_HEIGHT,
         10 + (ROW_OFFSET_HEIGHT * 2),
         10 + (ROW_OFFSET_HEIGHT * 1),
         10
     };
 
     for (unsigned int row = 0; row < SLOTS_IN_INVENTORY / SLOTS_IN_ROW; row++) {
-        inventory_renderRow(inventory, row * SLOTS_IN_ROW, ROW_Y_VALUES[row], selected);
+        inventory_renderRow(inventory, row * SLOTS_IN_ROW, ROW_Y_VALUES[row], selected, source);
+    }
+}
+
+void inventory_handleSelection(int key, int* slot) {
+    switch (key) {
+        case KEY_UP:
+            if (*slot > SLOTS_IN_INVENTORY - SLOTS_IN_ROW - 1) {
+                break;
+            }
+
+            *slot += SLOTS_IN_ROW;
+            break;
+
+        case KEY_DOWN:
+            if (*slot < SLOTS_IN_ROW) {
+                break;
+            }
+
+            *slot -= SLOTS_IN_ROW;
+            break;
+
+        case KEY_LEFT:
+            if (*slot % SLOTS_IN_ROW == 0) {
+                if (*slot > SLOTS_IN_INVENTORY - SLOTS_IN_ROW - 1) {
+                    break;
+                }
+
+                *slot += (2 * SLOTS_IN_ROW) - 1;
+
+                break;
+            }
+
+            (*slot)--;
+            break;
+
+        case KEY_RIGHT:
+            if (*slot % SLOTS_IN_ROW == SLOTS_IN_ROW - 1) {
+                if (*slot < SLOTS_IN_ROW) {
+                    break;
+                }
+
+                *slot -= (2 * SLOTS_IN_ROW) - 1;
+
+                break;
+            }
+
+            (*slot)++;
+            break;
+
+        case KEY_F1:
+        case KEY_F2:
+        case KEY_F3:
+        case KEY_F4:
+        case KEY_F5:
+        case KEY_F6:
+            *slot = key - KEY_F1;
+            break;
     }
 }
 
 void inventory_open(Inventory* inventory) {
     int selectedSlot = inventory->selectedHotbarSlot;
+    int sourceSlot = -1;
 
     while (true) {
-        inventory_renderSurvival(*inventory, selectedSlot);
+        inventory_renderSurvival(*inventory, selectedSlot, sourceSlot);
 
         dupdate();
 
-        switch (getkey().key) {
+        int key = getkey().key;
+
+        switch (key) {
             case KEY_EXIT:
                 goto closeInventory;
 
-            case KEY_UP:
-                selectedSlot += SLOTS_IN_ROW;
-                break;
+            case KEY_EXE:
+                if (sourceSlot >= 0) {
+                    InventorySlot temp = inventory->slots[selectedSlot];
 
-            case KEY_DOWN:
-                selectedSlot -= SLOTS_IN_ROW;
-                break;
+                    inventory->slots[selectedSlot] = inventory->slots[sourceSlot];
+                    inventory->slots[sourceSlot] = temp;
 
-            case KEY_LEFT:
-                selectedSlot--;
+                    sourceSlot = -1;
 
-                if (selectedSlot % SLOTS_IN_ROW == SLOTS_IN_ROW - 1) {
-                    selectedSlot += SLOTS_IN_ROW * 2;
+                    break;
                 }
 
+                sourceSlot = selectedSlot;
                 break;
 
-            case KEY_RIGHT:
-                selectedSlot++;
-
-                if (selectedSlot % SLOTS_IN_ROW == 0) {
-                    selectedSlot -= SLOTS_IN_ROW * 2;
-                }
-
-                break;
-
-            case KEY_F1:
-                selectedSlot = 0;
-                break;
-
-            case KEY_F2:
-                selectedSlot = 1;
-                break;
-
-            case KEY_F3:
-                selectedSlot = 2;
-                break;
-
-            case KEY_F4:
-                selectedSlot = 3;
-                break;
-
-            case KEY_F5:
-                selectedSlot = 4;
-                break;
-
-            case KEY_F6:
-                selectedSlot = 5;
+            default:
+                inventory_handleSelection(key, &selectedSlot);
                 break;
         }
 
--- src/inventory.h
+++ src/inventory.h
@@ -3,6 +3,8 @@
 
 #define SLOTS_IN_ROW 6
 #define SLOTS_IN_INVENTORY 24
+#define SLOT_WIDTH 12
+#define SLOT_HEIGHT 12
 
 typedef struct {
     unsigned int type;
@@ -17,10 +19,11 @@ typedef struct {
 Inventory inventory_default();
 
 void inventory_renderItem(int x, int y, unsigned int type);
-void inventory_renderRow(Inventory inventory, unsigned int offset, int y, int selected);
+void inventory_renderRow(Inventory inventory, unsigned int offset, int y, int selected, int source);
 void inventory_renderHotbar(Inventory inventory);
-void inventory_renderSurvival(Inventory inventory, int selected);
+void inventory_renderSurvival(Inventory inventory, int selected, int source);
 
+void inventory_handleSelection(int key, int* slot);
 void inventory_open(Inventory* inventory);
 
 #endif
